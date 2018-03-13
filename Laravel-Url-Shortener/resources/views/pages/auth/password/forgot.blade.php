33eb119d8123dfb97c41d2ad6f2e3ef616477edb
Louis Doyle <LouisDoyleOnline@gmail.com>
2018-03-13 23:18:43 +0000

    Basic Pages

---
+++ Laravel-Url-Shortener/resources/views/pages/auth/password/forgot.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Forgot Password')
+
+@section('body')
+
+@endsection
---
+++ Laravel-Url-Shortener/resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('body')
+
+@endsection
