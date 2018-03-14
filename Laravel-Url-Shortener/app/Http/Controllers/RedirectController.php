7f7076848973c450e21f25540b5e1caf6f12b73c
Louis Doyle <LouisDoyleOnline@gmail.com>
2018-03-14 14:57:51 +0000

    Redirect

--- Laravel-Url-Shortener/app/Http/Controllers/RedirectController.php
+++ Laravel-Url-Shortener/app/Http/Controllers/RedirectController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Url;
+use Redirect;
+use View;
+
 class RedirectController extends Controller
 {
     /**
      * @param string $token
+     * @return $this|\Illuminate\Http\RedirectResponse
      */
     public function getRedirect(string $token)
     {
-        //
+        $url = Url::whereToken($token)->get();
+
+        if ($url->count()) {
+            $url[0]->views++;
+
+            $url[0]->save();
+
+            return Redirect::away($url[0]->url);
+        }
+
+        return View::make('pages.massive-error')->with([
+            'alerts' => [
+                'red' => [
+                    ['The supplied redirect token is invalid.',],
+                ],
+            ],
+        ]);
     }
 }
