bb22a8cfe898a68432bdee29b62a01f544bb7cdb
Louis Doyle <LouisDoyleOnline@gmail.com>
2018-03-14 17:07:35 +0000

    Reset Password

--- Laravel-Url-Shortener/app/Http/Controllers/Auth/Password/ResetController.php
+++ Laravel-Url-Shortener/app/Http/Controllers/Auth/Password/ResetController.php
@@ -4,25 +4,126 @@ namespace App\Http\Controllers\Auth\Password;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
+use App\PasswordReset;
+use View;
+use Carbon\Carbon;
+use Redirect;
+use Validator;
+use Illuminate\Validation\ValidationException;
+use Hash;
 
 class ResetController extends Controller
 {
     /**
      * @param string $email
      * @param string $token
+     * @return $this|\Illuminate\Contracts\View\View
      */
     public function getReset(string $email, string $token)
     {
-        //
+        $user = User::whereEmail($email)->get();
+
+        if ($user->count()) {
+            $passwordReset = PasswordReset::whereUserAndToken($user[0], $token)->get();
+
+            if ($passwordReset->count()) {
+                if ($passwordReset[0]->usable) {
+                    if (Carbon::now()->timestamp - $passwordReset[0]->created_at->timestamp < 86400) {
+                        return View::make('pages.auth.password.reset')->with([
+                            'email' => $email,
+                            'token' => $token,
+                        ]);
+                    } else {
+                        return View::make('pages.massive-error')->with([
+                            'alerts' => [
+                                'red' => [
+                                    ['The supplied token has expired.',],
+                                ],
+                            ],
+                        ]);
+                    }
+                } else {
+                    return View::make('pages.massive-error')->with([
+                        'alerts' => [
+                            'red' => [
+                                ['The supplied token has already been used.',],
+                            ],
+                        ],
+                    ]);
+                }
+            } else {
+                return View::make('pages.massive-error')->with([
+                    'alerts' => [
+                        'red' => [
+                            ['The supplied token doesn\'t belong to a registered password reset.',],
+                        ],
+                    ],
+                ]);
+            }
+        } else {
+            return View::make('pages.massive-error')->with([
+                'alerts' => [
+                    'red' => [
+                        ['The supplied email doesn\'t belong to a registered user.',],
+                    ],
+                ],
+            ]);
+        }
     }
 
     /**
      * @param Request $request
      * @param string $email
      * @param string $token
+     * @return $this|\Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
      */
     public function postReset(Request $request, string $email, string $token)
     {
-        //
+        $user = User::whereEmail($email)->get();
+
+        if ($user->count()) {
+            $passwordReset = PasswordReset::whereUserAndToken($user[0], $token)->get();
+
+            if ($passwordReset->count()) {
+                if ($passwordReset[0]->usable) {
+                    if (Carbon::now()->timestamp - $passwordReset[0]->created_at->timestamp < 86400) {
+                        try {
+                            Validator::make($request->all(), [
+                                'password' => 'required|string|min:6|confirmed',
+                            ])->validate();
+                        } catch (ValidationException $exception) {
+                            return Redirect::route('auth.password.reset', ['email' => $email, 'token' => $token,])->with([
+                                'auth' => [
+                                    'password' => [
+                                        'reset' => [
+                                            'alerts' => [
+                                                'red' => $exception->errors(),
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ]);
+                        }
+
+                        $user[0]->password = Hash::make($request->all()['password']);
+                        $user[0]->save();
+
+                        $passwordReset[0]->usable = false;
+                        $passwordReset[0]->save();
+
+                        return View::make('pages.massive-success')->with([
+                            'alerts' => [
+                                'green' => [
+                                    ['Your password has been reset, you can now login with that password.',],
+                                ],
+                            ],
+                        ]);
+                    }
+                }
+            }
+        }
+
+        return Redirect::route('auth.password.reset', ['email' => $email, 'token' => $token,]);
     }
 }
--- Laravel-Url-Shortener/resources/views/pages/auth/password/reset.blade.php
+++ Laravel-Url-Shortener/resources/views/pages/auth/password/reset.blade.php
@@ -3,5 +3,77 @@
 @section('title', 'Reset Password')
 
 @section('body')
+    <div class="section">
+        <div class="grid">
+            <div class="grid-row">
+                <div class="grid-column-1-1 _align-center _justify-center">
+                    <div class="element-branding">
+                        <a href="{{ route('home') }}">Url<span>Shortener</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="section">
+        <div class="grid">
+            <div class="grid-row">
+                <div class="grid-column-1-1 _align-center _justify-center">
+                    <div class="element-text">
+                        <h1 class="_center">Reset Password</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="grid">
+            @if(Session::has('auth.password.reset.alerts.red'))
+                <div class="grid-row _margin-bottom-m">
+                    <div class="grid-column-1-1 _align-center _justify-center">
+                        <div class="element-alerts">
+                            <ul>
+                                @foreach(Session::get('auth.password.reset.alerts.red') as $item)
+                                    <li>{{ $item[0] }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            @endif
+            <div class="grid-row">
+                <div class="grid-column-1-1 _align-center _justify-center">
+                    <div class="element-form">
+                        <form action="{{ route('auth.password.reset', ['email' => $email, 'token' => $token]) }}" method="post" enctype="multipart/form-data">
+                            <label for="password">Password:</label>
+                            <input type="password" name="password" id="password" autofocus/>
+
+                            <label for="password_confirmation">Confirm Password:</label>
+                            <input type="password" name="password_confirmation" id="password_confirmation" autofocus/>
+
+                            {{ csrf_field() }}
+
+                            <input type="submit" name="submit" id="submit" value="Submit" class="_indigo"/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="grid">
+            <div class="grid-row">
+                <div class="grid-column-1-1 _align-center _justify-center">
+                    <div class="element-text">
+                        <div class="_center">Laravel Url Shortener</div>
+                        <div class="_center">
+                            <a href="https://github.com/LouisDoyle/Laravel-Url-Shortener" target="_blank">GitHub</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
