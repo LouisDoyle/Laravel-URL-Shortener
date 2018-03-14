b5af95e0bb2fab4eb11fc928b5143b34a519c5b5
Louis Doyle <LouisDoyleOnline@gmail.com>
2018-03-14 14:01:15 +0000

    Home (Guest)

--- Laravel-Url-Shortener/app/Http/Controllers/HomeController.php
+++ Laravel-Url-Shortener/app/Http/Controllers/HomeController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use View;
+
 class HomeController extends Controller
 {
     /**
-     *
+     * @return \Illuminate\Contracts\View\View
      */
     public function getHome()
     {
-        //
+        if (Auth::check()) {
+            //
+        }
+
+        return View::make('pages.home_guest');
     }
 }
--- Laravel-Url-Shortener/resources/views/pages/home_guest.blade.php
+++ Laravel-Url-Shortener/resources/views/pages/home_guest.blade.php
@@ -3,5 +3,72 @@
 @section('title', 'Home')
 
 @section('body')
+    <div class="section">
+        <div class="grid">
+            <div class="grid-row">
+                <div class="grid-column-1-1 _align-center _justify-center">
+                    <div class="element-branding">
+                        <a href="{{ route('home') }}">Url<span>Shortener</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="section">
+        <div class="grid">
+            <div class="grid-row">
+                <div class="grid-column-1-1 _align-center _justify-center">
+                    <div class="element-text">
+                        <h1 class="_center">Home</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="grid">
+            <div class="grid-row _margin-bottom-m">
+                <div class="grid-column-1-1 _align-center _justify-center">
+                    <div class="element-buttons">
+                        <ul>
+                            <li>
+                                <a href="{{ route('auth.register') }}" class="_indigo">Register</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('auth.login') }}" class="_indigo">Login</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="grid-row">
+                <div class="grid-column-1-1 _align-center _justify-center">
+                    <div class="element-buttons">
+                        <ul>
+                            <li>
+                                <a href="{{ route('auth.password.forgot') }}" class="_indigo _light">Forgot Your Password?</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="grid">
+            <div class="grid-row">
+                <div class="grid-column-1-1 _align-center _justify-center">
+                    <div class="element-text">
+                        <div class="_center">Laravel Url Shortener</div>
+                        <div class="_center">
+                            <a href="https://github.com/LouisDoyle/Laravel-Url-Shortener" target="_blank">GitHub</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
