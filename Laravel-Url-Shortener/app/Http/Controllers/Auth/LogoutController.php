3db9ded47f1991108d652ab8b52379f7b291005e
Louis Doyle <LouisDoyleOnline@gmail.com>
2018-03-14 14:32:17 +0000

    Logout

--- Laravel-Url-Shortener/app/Http/Controllers/Auth/LogoutController.php
+++ Laravel-Url-Shortener/app/Http/Controllers/Auth/LogoutController.php
@@ -3,22 +3,31 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Redirect;
+use Auth;
+use Session;
 
 class LogoutController extends Controller
 {
     /**
-     *
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function getLogout()
     {
-        //
+        return Redirect::route('home');
     }
 
     /**
-     *
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function postLogout()
     {
-        //
+        if (Auth::check()) {
+            Auth::logout();
+
+            Session::invalidate();
+        }
+
+        return Redirect::route('home');
     }
 }
